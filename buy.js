e4e2f7221ef245ea0cecec30760fd3383027a47f
Luna Seifert <thepuddingdeity@gmail.com>
2022-09-17 09:25:07 -0600

    Sorted out all of the 'cart/buy' code. Started beautifying a little more.

--- buy.js
+++ buy.js
@@ -1,15 +1,30 @@
+document.getElementById("cart").addEventListener('click', function(){
+    if (productNames[0] === undefined){
+        alert("Empty Cart.");
+    }
+    else{
+        totalPrice = 0;
+        productPrices.forEach(nyan => {totalPrice = totalPrice + parseFloat(nyan)});
+        alert("Products:" + productNames + `\n Total Price: $${totalPrice.toFixed(2)}`);
+    }
+});
+
 const productNames = [];
 const productPrices = [];
 let totalPrice = 0;
 
 document.addEventListener("keypress", function(b){
     if (b.key === "b"){
-        //Buy logic
-        //Test code to move to a one-time button
-        productPrices.forEach(nyan => {totalPrice = totalPrice + parseFloat(nyan)});
-        alert(totalPrice.toFixed(2));
+        if(productNames[0] === undefined){
+            alert("Empty Cart.");
+        }
+        else {
+            totalPrice = 0;
+            productPrices.forEach(nyan => {totalPrice = totalPrice + parseFloat(nyan)});
+            alert("$" + totalPrice.toFixed(2));
+        }
     }
-})
+});
 
 
 //The below is just test code again
@@ -17,5 +32,7 @@ function clicked(clicked_id)
   {
       alert(clicked_id);
       let itemPrice = clicked_id.slice(clicked_id.indexOf("$") +1);
+      let itemName = clicked_id.slice(0, clicked_id.indexOf("$") - 1);
       productPrices.push(itemPrice);
+      productNames.push(" " + itemName);
   }

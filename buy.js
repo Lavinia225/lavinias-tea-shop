93b5f2a7711d12a8dd0ed1adef1e0d6d146a1e02
Luna Seifert <thepuddingdeity@gmail.com>
2022-09-17 11:34:08 -0600

    Added cookies so the cart doesn't empty when you go to other pages.

--- buy.js
+++ buy.js
@@ -35,4 +35,23 @@ function clicked(clicked_id)
       let itemName = clicked_id.slice(0, clicked_id.indexOf("$") - 1);
       productPrices.push(itemPrice);
       productNames.push(" " + itemName);
+      document.cookie=`productPrices=${productPrices};Max-Age=86,400,000; Secure;`; //Max age is milliseconds in a day, or 1000 * 60 * 60 * 24
+      document.cookie=`productNames=${productNames};Max-Age=86,400,000; Secure;`;
   }
+
+  function cookieReader(){
+    const seperatedCookies = document.cookie.split(";"); //Divide price and name strings
+    const priceHolder = seperatedCookies[0].slice(seperatedCookies[0].indexOf("=") + 1).split(",") //Cut off the label at slice, then puts each price in an element
+    //const priceHolderSliced = priceHolder.split(",");
+    const nameHolder = seperatedCookies[1].slice(seperatedCookies[1].indexOf("=") + 1).split(",")
+
+    for (i = 0; i < priceHolder.length; i++){
+        productPrices.push(priceHolder[i]);
+    }
+
+    for (i = 0; i < nameHolder.length; i++){
+        productNames.push(nameHolder[i]);
+    }
+  }
+
+  cookieReader();
